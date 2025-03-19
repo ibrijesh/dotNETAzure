2a7fa3d649084dc77f6b3896b6d15a59c25d229a
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-03-19 13:17:39 +0530

    updated Hello, World endpoint

--- MicrosoftAzureForDevelopers/Program.cs
+++ MicrosoftAzureForDevelopers/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/persons", (AppDbContext db) => db.Persons.ToList());
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! " + app.Environment.EnvironmentName);
 
 app.UseHttpsRedirection();
 
