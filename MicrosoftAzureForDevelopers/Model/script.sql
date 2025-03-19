601f8aa5ddb851686c456edd1724c66a3c6ad792
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-03-19 13:17:16 +0530

    add Seed Person Data Script

---
+++ MicrosoftAzureForDevelopers/Model/script.sql
@@ -0,0 +1,55 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Persons] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NULL,
+    [DateOfBirth] datetime2 NOT NULL,
+    CONSTRAINT [PK_Persons] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250319045556_AddPersonTable', N'9.0.3');
+
+DECLARE @var sysname;
+SELECT @var = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Persons]') AND [c].[name] = N'LastName');
+IF @var IS NOT NULL EXEC(N'ALTER TABLE [Persons] DROP CONSTRAINT [' + @var + '];');
+ALTER TABLE [Persons] ALTER COLUMN [LastName] nvarchar(100) NULL;
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Persons]') AND [c].[name] = N'FirstName');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Persons] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [Persons] ALTER COLUMN [FirstName] nvarchar(100) NOT NULL;
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'DateOfBirth', N'FirstName', N'LastName') AND [object_id] = OBJECT_ID(N'[Persons]'))
+    SET IDENTITY_INSERT [Persons] ON;
+INSERT INTO [Persons] ([Id], [DateOfBirth], [FirstName], [LastName])
+VALUES (1, '1990-05-15T00:00:00.0000000', N'John', N'Doe'),
+(2, '1985-03-22T00:00:00.0000000', N'Jane', N'Smith'),
+(3, '2000-07-10T00:00:00.0000000', N'Alex', N'Johnson'),
+(4, '1992-11-04T00:00:00.0000000', N'Emily', N'Davis'),
+(5, '1980-08-30T00:00:00.0000000', N'Michael', N'Brown');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'DateOfBirth', N'FirstName', N'LastName') AND [object_id] = OBJECT_ID(N'[Persons]'))
+    SET IDENTITY_INSERT [Persons] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250319054839_SeedPersonData', N'9.0.3');
+
+COMMIT;
+GO
+
